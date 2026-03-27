a78d12dad7d725011055183d8bb5e34a398e55de
Amandeep Kochhar <amandeep.netweb@gmail.com>
2026-03-27 16:58:28 +0530

    Fixed bugs

--- src/components/files/index.ts
+++ src/components/files/index.ts
@@ -2,7 +2,4 @@ export { FileCard } from "./FileCard";
 export { FileList } from "./FileList";
 export { FileUploader } from "./FileUploader";
 export { FileViewer } from "./FileViewer";
-export { PDFViewer } from "./PDFViewer";
-export { DocxViewer } from "./DocxViewer";
-export { TxtViewer } from "./TxtViewer";
 export { TagInput } from "./TagInput";
